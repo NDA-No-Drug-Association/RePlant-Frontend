218cf9c1cca635ebe0a41af166d732131c1d8677
castle <castle.h0326@gmail.com>
2025-07-17 13:56:10 +0900

    style: folder구조 변경

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/Replant_logo.png', width: 300, height: 300),
+              const SizedBox(height: 10),
+              const Text(
+                '다시 살아갈 당신에게 희망을 🍀',
+                style: TextStyle(fontSize: 18, color: Colors.black87),
+              ),
+              const SizedBox(height: 120),
+              // 카카오 로그인 버튼
+              SizedBox(
+                width: 320,
+                height: 56,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                    padding: EdgeInsets.zero,
+                  ),
+                  child: Image.asset(
+                    'assets/kakao_login.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              // 네이버 로그인 버튼
+              SizedBox(
+                width: 320,
+                height: 56,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                    padding: EdgeInsets.zero,
+                  ),
+                  child: Image.asset(
+                    'assets/Naver_login.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
